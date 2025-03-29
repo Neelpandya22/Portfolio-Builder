838c857643b82eefc8e32c604822957fff080127
Neel Pandya <149610480+Neelpandya22@users.noreply.github.com>
2025-03-29 12:16:37 +0530

    Update Portfolio.mjs

--- models/Portfolio.mjs
+++ models/Portfolio.mjs
@@ -1,1 +1,10 @@
+import mongoose from "mongoose";
 
+const PortfolioSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  title: String,
+  description: String,
+  createdAt: { type: Date, default: Date.now },
+});
+
+export default mongoose.model("Portfolio", PortfolioSchema);
